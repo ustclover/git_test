845e685a14accbbb59da38d3390d075492ae00f0
mengjie <mengjie@wondersgroup.com>
2022-05-02 15:29:31 +0800

    second commit

--- src/main/java/com/wonders/git_test/GItTest.java
+++ src/main/java/com/wonders/git_test/GItTest.java
@@ -3,6 +3,6 @@ package com.wonders.git_test;
 public class GItTest {
     public static void main(String[] args) {
         System.out.println("This is Git");
-        System.out.println("sss");
+        System.out.println("This is Git1");
     }
 }
