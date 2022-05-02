bdc458340b0219d39ff04fffa917128cb17ab734
mengjie <mengjie@wondersgroup.com>
2022-05-02 15:30:38 +0800

    third commit

--- src/main/java/com/wonders/git_test/GItTest.java
+++ src/main/java/com/wonders/git_test/GItTest.java
@@ -2,7 +2,8 @@ package com.wonders.git_test;
 
 public class GItTest {
     public static void main(String[] args) {
-        System.out.println("This is Git");
         System.out.println("This is Git1");
+        System.out.println("This is Git2");
+        System.out.println("This is Git3");
     }
 }
